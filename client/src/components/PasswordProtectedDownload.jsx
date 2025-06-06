bdd35aa0683d570b3a6df123234eaac8b7ca9a71
TrixieDreamz <trundle24@yahoo.com>
2025-06-06 09:32:07 +0200

    Frontend Ready for Production

--- client/src/components/PasswordProtectedDownload.jsx
+++ client/src/components/PasswordProtectedDownload.jsx
@@ -11,7 +11,7 @@ export default function PasswordProtectedDownload() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/download", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/download`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
